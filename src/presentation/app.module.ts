0d5d14a685bc6fc76aa6bf20590003433b09c066
Nadir Gherbi <nadir.gherbi@bcsoft.net>
2022-07-17 19:27:17 +0200

    Rearrange dependencies

--- src/presentation/app.module.ts
+++ src/presentation/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
+import { CronModule } from 'src/infrastructure/CronServices/cronService.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CronModule } from './CronServices/cronService.module';
+// import { CronModule } from './infrastructure/CronServices/cronService.module';
 
 @Module({
     imports: [ScheduleModule.forRoot(), CronModule],
