77a3ceae9399a2594b80b9b17d59f2dbdfd88378
Nadir Gherbi <nadir.gherbi@bcsoft.net>
2022-07-17 21:49:47 +0200

    Add basic tweet strem reader

--- src/infrastructure/CronServices/TestCronService.ts
+++ src/infrastructure/CronServices/TestCronService.ts
@@ -1,12 +1,40 @@
 import { Injectable, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { Cron, CronExpression } from '@nestjs/schedule';
+import { ETwitterStreamEvent, TwitterApi } from 'twitter-api-v2';
 
 @Injectable()
 export class TasksService {
+
+    constructor(private readonly configService: ConfigService) { }
     private readonly logger = new Logger(TasksService.name);
 
-    @Cron(CronExpression.EVERY_10_SECONDS)
-    handleCron() {
-        this.logger.debug('Called every 10 seconds');
+    @Cron(new Date(Date.now() + 10 * 1000))
+    async handleCron() {
+        this.logger.debug('Cron Job HERE!');
+        const client = new TwitterApi(this.configService.get<string>('twitterBearerToken'));
+
+        const rules = await client.v2.streamRules();
+        if (rules.data?.length) {
+            await client.v2.updateStreamRules({
+                delete: { ids: rules.data.map(rule => rule.id) },
+            });
+        }
+        await client.v2.updateStreamRules({
+            add: [{ value: 'JavaScript' }, { value: 'NodeJS' }],
+        });
+
+        const stream = await client.v2.searchStream({
+            'tweet.fields': ['referenced_tweets', 'author_id'],
+            expansions: ['referenced_tweets.id'],
+        });
+        stream.autoReconnect = true;
+        stream.on(ETwitterStreamEvent.Data, async tweet => {
+            const isARt = tweet.data.referenced_tweets?.some(tweet => tweet.type === 'retweeted') ?? false;
+            if (isARt) {
+                return;
+            }
+            console.log(tweet.data?.text);
+        });
     }
 }
--- src/presentation/app.controller.ts
+++ src/presentation/app.controller.ts
@@ -1,12 +1,9 @@
 import { Controller, Get, Logger } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { TasksService } from 'src/infrastructure/CronServices/TestCronService';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-    constructor(private readonly appService: AppService, private readonly configService: ConfigService) { }
-    private readonly logger = new Logger(TasksService.name);
+    constructor(private readonly appService: AppService) { }
     @Get()
     getHello(): string {
         return this.appService.getHello();
