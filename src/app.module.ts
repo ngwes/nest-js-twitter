d3acf63f50cf5c049b5218d24fe0417aec6328db
Nadir Gherbi <nadir.gherbi@bcsoft.net>
2022-07-17 19:23:46 +0200

    Add cron jobs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CronModule } from './CronServices/cronService.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [ScheduleModule.forRoot(), CronModule],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
